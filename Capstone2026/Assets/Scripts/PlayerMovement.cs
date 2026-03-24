9341cd94eab3b6bbdfd3916531432344766d1f4c
Olivia <83168370+OliviaGoBrrr@users.noreply.github.com>
2026-03-24 21:35:58 +1000

    Player movement, jumping, gravity implemented

--- Capstone2026/Assets/Scripts/PlayerMovement.cs
+++ Capstone2026/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 using NUnit.Framework.Constraints;
+using Unity.VisualScripting;
 
 [RequireComponent(typeof(PlayerInput),typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
@@ -11,12 +12,14 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 2f;
 
     private Vector3 move;
-    private Vector3 velocity;
+    public Vector3 velocity;
     private float gravity = 9.82f;
     private float jumpTimeOffset = 0.2f;
+    CapsuleCollider groundCheckCollider;
+    float groundedTimer;
     float jumpTimer;
     float startHeight;
-    bool isGrounded;
+    public bool isGrounded;
     bool isJumping;
 
 
@@ -36,34 +39,57 @@ public class PlayerMovement : MonoBehaviour
             playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
         }
         playerInput = GetComponent<PlayerInput>();
+        groundCheckCollider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
     }
 
     private void Update()
     {
+        isGrounded = IsGrounded();
+    }
+
+    private void FixedUpdate()
+    {
         PlayerMove();
+        PlayerJump();
     }
 
     public void OnJump()
     {
-        if (IsGrounded())
+        if (IsGrounded() && jumpTimer <= 0.0f)
         {
+            groundedTimer = 0.1f;
             velocity.y += jumpHeight;
-            isJumping = true;
+            jumpTimer = 0.2f;
         }
-        Debug.Log("Player pressed the jump key");
     }
 
-    bool IsGrounded() { return Physics.Raycast(transform.position, -Vector3.up, 0.1f); }
+    bool IsGrounded()
+    {
+        if (groundedTimer <= 0.0f)
+        {
+            Vector3 groundCheck = transform.position;
+            float distanceToGround = groundCheckCollider.bounds.extents.y;
+            bool blah = Physics.Raycast(groundCheck, -Vector3.up, distanceToGround + 0.01f);
+            Debug.DrawRay(groundCheck, -Vector3.up, Color.blue);
+            return blah;
+        }
+        else
+        {
+           return false;
+        }
+    }
+
 
     private void PlayerJump()
     {
-        if (isJumping)
-        {
-            Vector3 newPos = transform.position;
-            move.y += gravity * Time.deltaTime;
-            rb.MovePosition(newPos);
-        }
+        jumpTimer -= Time.fixedDeltaTime;
+        groundedTimer -= Time.fixedDeltaTime;
     }
 
     // Player Horizontal Movement
@@ -76,9 +102,20 @@ public class PlayerMovement : MonoBehaviour
     private void PlayerMove()
     {
         Vector3 newPos = transform.position;
-        newPos.x += velocity.x * moveSpeed * Time.deltaTime;
-        newPos.z += velocity.z * moveSpeed * Time.deltaTime;
 
+        if (!IsGrounded())
+        {
+            velocity.y -= gravity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        velocity.y = Mathf.Clamp(velocity.y, -3f, 10f);
+
+        newPos += velocity * moveSpeed * Time.fixedDeltaTime;
+        
         rb.MovePosition(newPos);
     }
 
