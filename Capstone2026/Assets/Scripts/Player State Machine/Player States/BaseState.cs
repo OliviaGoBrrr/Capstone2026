c063b91739eb143b11e3a5f88d382e8170dd0db1
Jamie Robinson <162258729+Necromancerrrr@users.noreply.github.com>
2026-03-30 18:45:01 +1000

    fixed state error

--- Capstone2026/Assets/Scripts/Player State Machine/Player States/BaseState.cs
+++ Capstone2026/Assets/Scripts/Player State Machine/Player States/BaseState.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BaseState : PlayerState
+public class BaseState
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
