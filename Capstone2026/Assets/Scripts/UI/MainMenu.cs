cb6d25ba4cf5604055995d9062eb763321b84e2e
Jamie Robinson <jamiegribblerobinson@gmail.com>
2026-04-20 12:37:54 +1000

    Main menu settings menu

--- Capstone2026/Assets/Scripts/UI/MainMenu.cs
+++ Capstone2026/Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,7 +10,12 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject settingsOptions;
     [SerializeField] private GameObject settingsButton;
 
+    [SerializeField] private GameObject mainCamera;
+
     private bool settingsShown = false;
+
+    [SerializeField] private Transform cameraSettingsPos;
+    [SerializeField] private Transform cameraNormalPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +31,11 @@ public class MainMenu : MonoBehaviour
         {
             if (settingsShown)
             {
+                // kill all tweens relating to the camera
+                DOTween.Kill("Camera");
+
+                mainCamera.transform.DOMove(cameraNormalPos.position, 0.5f);
+
                 settingsShown = false;
                 settingsOptions.SetActive(false);
                 settingsButton.SetActive(true);
@@ -42,6 +53,11 @@ public class MainMenu : MonoBehaviour
 
     public void SettingsButtonPressed()
     {
+        // kill all tweens relating to the camera
+        DOTween.Kill("Camera");
+
+        mainCamera.transform.DOMove(cameraSettingsPos.position, 0.5f);
+
         settingsShown = true;
         settingsOptions.SetActive(true);
         settingsButton.SetActive(false);
@@ -49,6 +65,11 @@ public class MainMenu : MonoBehaviour
 
     public void SettingsBackButtonPressed()
     {
+        // kill all tweens relating to the camera
+        DOTween.Kill("Camera");
+
+        mainCamera.transform.DOMove(cameraNormalPos.position, 0.5f);
+
         settingsShown = false;
         settingsOptions.SetActive(false);
         settingsButton.SetActive(true);
