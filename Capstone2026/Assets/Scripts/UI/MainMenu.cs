5934e8e8bb097a19d6a7c039183b088f869577ee
Jamie Robinson <jamiegribblerobinson@gmail.com>
2026-05-07 05:58:41 +1000

    Main menu settings tv animation

--- Capstone2026/Assets/Scripts/UI/MainMenu.cs
+++ Capstone2026/Assets/Scripts/UI/MainMenu.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Text;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -19,14 +20,18 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject mainCamera;
 
     private bool settingsShown = false;
+    [SerializeField] private GameObject settingsBg;
+    [SerializeField] private GameObject turnOnEffect;
 
     [SerializeField] private Transform cameraSettingsPos;
     [SerializeField] private Transform cameraNormalPos;
 
+    private bool currentlyAnimating = false;
+
     // AUDIO CLIPS
     [SerializeField] private AudioClip buttonPressedClip;
 
-    void Start()
+    void Awake()
     {
         // kill all tweens relating to the camera
         DOTween.KillAll();
@@ -34,9 +39,13 @@ public class MainMenu : MonoBehaviour
 
         settingsOptions.SetActive(false);
         settingsButton.SetActive(true);
+        
 
         mainCamera.transform.position = cameraNormalPos.position;
         mainCamera.transform.rotation = cameraNormalPos.rotation;
+
+        settingsBg.SetActive(false);
+        turnOnEffect.SetActive(false);
     }
 
     // Update is called once per frame
@@ -46,15 +55,7 @@ public class MainMenu : MonoBehaviour
         {
             if (settingsShown)
             {
-                // kill all tweens relating to the camera
-                DOTween.Kill("Camera");
-
-                mainCamera.transform.DOMove(cameraNormalPos.position, 0.5f).SetId("Camera");
-                mainCamera.transform.DORotate(new Vector3(0, 0, 0), 0.5f).SetId("Camera");
-
-                settingsShown = false;
-                settingsOptions.SetActive(false);
-                settingsButton.SetActive(true);
+                SettingsBackButtonPressed();
             }
         }
     }
@@ -72,20 +73,40 @@ public class MainMenu : MonoBehaviour
     {
         // kill all tweens relating to the camera
         DOTween.Kill("Camera");
+        DOTween.Kill("ScreenOnOff");
+        currentlyAnimating = true;
         mainCamera.transform.DOMove(cameraSettingsPos.position, 0.5f).SetId("Camera");
-        mainCamera.transform.DORotate(new Vector3(-12, 0, -5), 0.5f).SetId("Camera");
+        mainCamera.transform.DORotate(new Vector3(-12, 0, -5), 0.5f).SetId("Camera").OnComplete(() =>
+        {
+            turnOnEffect.SetActive(true);
+            turnOnEffect.GetComponent<Image>().color = Color.white;
+            turnOnEffect.transform.localScale = new Vector3(0f, 0.01f, 0f);
+            turnOnEffect.transform.DOScale(new Vector3(1f, 0.01f, 1f), 0.05f).SetId("ScreenOnOff").OnComplete(() =>
+            {
+                turnOnEffect.transform.DOScale(new Vector3(1f, 1f, 1f), 0.05f).SetId("ScreenOnOff").OnComplete(() =>
+                {
+                    settingsBg.SetActive(true);
+                    turnOnEffect.GetComponent<Image>().DOFade(0f, 0.2f);
+                    currentlyAnimating = false;
+                });
+            });
 
-        //AudioManager.Instance.PlaySFX(buttonPressedClip, mainCamera.transform, 1);
+        });
+
+        AudioManager.Instance.PlaySFX(buttonPressedClip, mainCamera.transform, 1);
 
         settingsShown = true;
         settingsOptions.SetActive(true);
         settingsButton.SetActive(false);
+
+        
     }
 
     public void SettingsBackButtonPressed()
     {
         // kill all tweens relating to the camera
         DOTween.Kill("Camera");
+        DOTween.Kill("ScreenOnOff");
 
         mainCamera.transform.DOMove(cameraNormalPos.position, 0.5f).SetId("Camera");
         mainCamera.transform.DORotate(new Vector3(0, 0, 0), 0.5f).SetId("Camera");
@@ -93,6 +114,8 @@ public class MainMenu : MonoBehaviour
         settingsShown = false;
         settingsOptions.SetActive(false);
         settingsButton.SetActive(true);
+        settingsBg.SetActive(false);
+        turnOnEffect.SetActive(false);
     }
 
     public void ExitButtonPressed()
