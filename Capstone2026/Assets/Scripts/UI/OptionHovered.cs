46fa0d42e6d10702e17087f1409be1c4eb638d46
Jamie Robinson <jamiegribblerobinson@gmail.com>
2026-05-21 05:20:15 +1000

    UI options filled out + partially styled

---
+++ Capstone2026/Assets/Scripts/UI/OptionHovered.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
+
+public class OptionHovered : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Outline backgroundOutline;
+
+    [SerializeField] private InputActionReference pauseAction;
+
+    void Start()
+    {
+        backgroundOutline.enabled = false;
+    }
+
+    void OnEnable()
+    {
+        backgroundOutline.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        backgroundOutline.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        backgroundOutline.enabled = false;
+    }
+
+}
