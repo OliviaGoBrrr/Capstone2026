ce77539bdf06b37e0c9d99be9e78a78911fb2369
maeveronii <maevechamberlain27@gmail.com>
2026-05-21 16:15:42 +1000

    track checkpoint times

--- Capstone2026/Assets/Scripts/Checkpoint.cs
+++ Capstone2026/Assets/Scripts/Checkpoint.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public PlayerManager playerM;
+    public int checkpointNumber;
+    public bool activated = false;
+
+    public void Awake()
+    {
+        playerM = FindFirstObjectByType<PlayerManager>().GetComponent<PlayerManager>(); 
+    }
 
     public void OnTriggerEnter(Collider other)
     {
+        if (!activated)
+        {
+            PostGameDataLog.updateCheckpointTimes($"Checkpoint: {checkpointNumber}" + $"{Time.timeSinceLevelLoad}");
+        }
+
+        activated = true;
+        
         if(playerM.solarPanel.isInLight) // make sure that the checkpoint is acutally in light
         {
             playerM.lastCheckpoint = this.transform.position;
--- Capstone2026/Assets/Scripts/PostGameDataLog.cs
+++ Capstone2026/Assets/Scripts/PostGameDataLog.cs
@@ -5,10 +5,22 @@ public static class PostGameDataLog
 {
     [Header("During Game Info")]
     static public List<string> timeInEachScene = new List<string>();
+    static public List<string> timeToReachEachCheckpoint = new List<string>();
+    static void Awake()
+    {
+        timeInEachScene.Add("Time in each scene:");
+        timeToReachEachCheckpoint.Add("Time to reach each checkpoint:");
+    }
     static public void updateSceneTime(string timeSpent)
     {
         timeInEachScene.Add(timeSpent);
         Debug.Log(string.Join("\n", timeInEachScene));
+    }
+
+    static public void updateCheckpointTimes(string timeSpent)
+    {
+        timeToReachEachCheckpoint.Add(timeSpent);
+        Debug.Log(string.Join("\n", timeToReachEachCheckpoint));
     }
 
     static public void sendInfoToDiscord()
