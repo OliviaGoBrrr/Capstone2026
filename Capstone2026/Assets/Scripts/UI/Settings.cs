e0248b4d6ff478ef7bc40b6db693bc475c8c77bb
maeveronii <maevechamberlain27@gmail.com>
2026-06-20 17:28:32 +1000

    fov slider functionality

---
+++ Capstone2026/Assets/Scripts/UI/Settings.cs
@@ -0,0 +1,17 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [Header("Objects")]
+    public CinemachineCamera cam;
+
+    [Header("UI Objects")]
+    [SerializeField] private UnityEngine.UI.Slider FOVslider;
+
+    public void updateFOV()
+    {
+        cam.Lens.FieldOfView = FOVslider.value;
+    }
+
+}
