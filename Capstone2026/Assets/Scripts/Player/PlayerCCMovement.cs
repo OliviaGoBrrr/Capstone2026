c2f1ac7cfc3eb85649d0712f2009b69ba2f0635a
Olivia <83168370+OliviaGoBrrr@users.noreply.github.com>
2026-07-27 13:55:39 +1000

    Updated to fix some rotation issues

--- Capstone2026/Assets/Scripts/Player/PlayerCCMovement.cs
+++ Capstone2026/Assets/Scripts/Player/PlayerCCMovement.cs
@@ -130,8 +130,6 @@ public class PlayerCCMovement : MonoBehaviour
     {
         groundedPlayer = playerController.isGrounded;
 
-        //playerController.Move(playerVelocity * Time.deltaTime);
-
         MovePlayer();
 
         HandleGravity();
@@ -187,42 +185,16 @@ public class PlayerCCMovement : MonoBehaviour
         Vector3 camRightXProduct = vectorToRotate.x * camR;
 
         Vector3 vectorRoatatedToCameraSpace = camForwardZProduct + camRightXProduct;
-        vectorRoatatedToCameraSpace.y = currentYValue;
-        return vectorRoatatedToCameraSpace;
-    }
-    
 
-    public void PlayerMove()
-    {
-        // Get the x,z direction the player is inputting
-        Vector2 input = moveAction.action.ReadValue<Vector2>();
-
-        Vector2 inputNorm = input.normalized;
-
-        // Rotate the player
-        Vector3 camF = playerCamera.transform.forward;
-        Vector3 camR = playerCamera.transform.right;
-
-        camF.y = 0f;
-        camR.y = 0f;
-
-        desiredMove = (camF * input.y + camR * input.x).normalized;
-
-        // Jump & Fall States
-
-        // Rotates the player if they're inputting an action
-        if(desiredMove != Vector3.zero)
+        if (vectorRoatatedToCameraSpace != Vector3.zero)
         {
-            RotatePlayer(desiredMove);
+            RotatePlayer(vectorRoatatedToCameraSpace.normalized);
         }
 
-        Vector3 targetVelcoity = desiredMove * moveSpeed;
+        vectorRoatatedToCameraSpace.y = currentYValue;
 
-        playerVelocity = Vector3.MoveTowards(playerVelocity, new Vector3(targetVelcoity.x, playerVelocity.y, targetVelcoity.z), acceleration * Time.deltaTime);
+        return vectorRoatatedToCameraSpace;
     }
-
-
-
 
     public void PlayerJump()
     {
@@ -433,4 +405,37 @@ public class PlayerCCMovement : MonoBehaviour
         grappleAction.action.Disable();
         runAction.action.Disable();
     }
+
+
+    /* LEGACY MOVEMENT 
+public void PlayerMove()
+{
+    // Get the x,z direction the player is inputting
+    Vector2 input = moveAction.action.ReadValue<Vector2>();
+
+    Vector2 inputNorm = input.normalized;
+
+    // Rotate the player
+    Vector3 camF = playerCamera.transform.forward;
+    Vector3 camR = playerCamera.transform.right;
+
+    camF.y = 0f;
+    camR.y = 0f;
+
+    desiredMove = (camF * input.y + camR * input.x).normalized;
+
+    // Jump & Fall States
+
+    // Rotates the player if they're inputting an action
+    if(desiredMove != Vector3.zero)
+    {
+        RotatePlayer(desiredMove);
+    }
+
+    Vector3 targetVelcoity = desiredMove * moveSpeed;
+
+    playerVelocity = Vector3.MoveTowards(playerVelocity, new Vector3(targetVelcoity.x, playerVelocity.y, targetVelcoity.z), acceleration * Time.deltaTime);
+}
+*/
+
 }
