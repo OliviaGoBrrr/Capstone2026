48325efef9ce99bc8c68cc0734d2848964840a9d
Helping-Gingers <163386874+Helping-Gingers@users.noreply.github.com>
2026-08-10 18:08:57 +1000

    Various Changes [in Description]
    Changed Scene Names for Consistency
    Fixed these tasks:
    
    Campfire in Dock has no collider
    Decrease the range at which LODs are at their highest detail
    Change Phinn to a Capsule Collider
    Update the motor collider, as it is not currently attached
    Can fall through the water plane in the dock
    Sign for post office/farm is floating - it needs to be lowered slightly
    The boulder in the farm does not have the text follow the player's position - this needs to be updated

--- Capstone2026/Assets/Scripts/UI/SceneLoader.cs
+++ Capstone2026/Assets/Scripts/UI/SceneLoader.cs
@@ -30,14 +30,13 @@ public class SceneLoader : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        if (currentScene.name == "MVP_Level-A")
+        if (currentScene.name == "_MVPFarmLevel")
         {
-            StartCoroutine(LoadScene("Dock"));
-
+            StartCoroutine(LoadScene("_MVPDock"));
         }
         else
         {
-            StartCoroutine(LoadScene("MVP_Level-A"));
+            StartCoroutine(LoadScene("_MVPFarmLevel-A"));
         }
     }
 }
