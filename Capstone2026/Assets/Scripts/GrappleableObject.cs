2a8a70418e3cbfa79d4f3d1335ff1225f1ecbbef
Jamie Robinson <jamiegribblerobinson@gmail.com>
2026-09-09 14:55:17 +1000

    Added outlines? they kinda buns

--- Capstone2026/Assets/Scripts/GrappleableObject.cs
+++ Capstone2026/Assets/Scripts/GrappleableObject.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class GrappleableObject : MonoBehaviour
 {
     public Transform anchorPoint;
-    public Canvas grappleUICanvas;
+    //public Canvas grappleUICanvas;
     private Transform player;
-
+    /*
     private void Awake()
     {
         if (grappleUICanvas == null)
@@ -18,6 +18,7 @@ public class GrappleableObject : MonoBehaviour
             grappleUICanvas.gameObject.SetActive(false);
         }
     }
+    
 
     private void LateUpdate()
     {
@@ -32,4 +33,5 @@ public class GrappleableObject : MonoBehaviour
         grappleUICanvas.transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, 
             Camera.main.transform.rotation * Vector3.up);
     }
+    */
 }
